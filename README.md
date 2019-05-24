27fb9b2fba44a6026ae45ab625c94e2174dd1796
matteo di paolantonio <matteodipaolantonio@matteos-MacBook-Pro.local>
2019-05-24 09:25:31 +0200

    First commit - systemiczation

---
+++ README.md
@@ -0,0 +1,3 @@
+# torralpoll-back
+
+
---
+++ config/default.js
@@ -0,0 +1,47 @@
+module.exports = {
+	server: {
+		host: '0.0.0.0',
+		port: 4000,
+	},
+	metrics: {
+		key: process.env.APPINSIGHTS_INSTRUMENTATIONKEY,
+		internalLogging: false,
+		context: {
+			tags: {
+				'ai.cloud.role': process.env.npm_package_name,
+				'ai.cloud.roleInstance': process.env.HOSTNAME || 'local',
+			},
+		},
+		autoCollect: {
+			requests: true,
+			performance: true,
+			exceptions: true,
+			dependencies: true,
+			console: false,
+		},
+	},
+	logger: {
+		transport: 'console',
+		include: [
+			'tracer',
+			'timestamp',
+			'level',
+			'message',
+			'error.message',
+			'error.code',
+			'error.stack',
+			'request.url',
+			'request.headers',
+			'request.params',
+			'request.method',
+			'response.statusCode',
+			'response.headers',
+			'response.time',
+			'process',
+			'system',
+			'package.name',
+			'service',
+		],
+		exclude: ['password', 'secret', 'token', 'request.headers.cookie', 'dependencies', 'devDependencies'],
+	},
+};
