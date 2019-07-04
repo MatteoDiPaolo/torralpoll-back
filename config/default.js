19bfb0a11978e30d5080c8e6e28ed2d000b692a6
matteo di paolantonio <matteodipaolantonio@matteos-MacBook-Pro.local>
2019-07-04 08:43:24 +0200

    Trying to make swagger work on heroku

--- config/default.js
+++ config/default.js
@@ -19,7 +19,7 @@ module.exports = {
 						version: '1.0.0',
 						contact: { email: 'matteo.dipaolantonio@guidesmiths.com' },
 					},
-					host: process.env.SERVICE_ENV !== 'local' ? process.env.HOST : `localhost:${process.env.PORT || 4000}`,
+					host: process.env.HOST || `localhost:${process.env.PORT || 4000}`,
 					basePath: '/',
 					produces: ['application/json'],
 					schemes: ['http', 'https'],
